1d571a414f6d69351486358af2e74277553aac80
Sandeep Vadla <sandeep_vadla@merilytics.com>
2023-12-17 17:20:00 +0530

    Install packages

---
+++ 04.packages.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=(id -u)
+
+VALIDATE(){
+    if {$1 -ne 0}
+    then
+         echo -e "2... FAILED"
+    else
+         echo -e "2...SUCESS"
+
+
+}
+if {$ID -ne 0 } 
+then
+    echo -e " run the script with root user"
+    exit 1
+else
+    echo -e " you are root user"
+
+for package $0
+do
+    yum list install $package
+    if {$? -ne 0} 
+    then 
+     yum install  package -y
+     VALIDATE {$1} install package
+
+    else
+    echo -e "$PACKAGE alreay installed.... SKIPPING"
+  
+done
