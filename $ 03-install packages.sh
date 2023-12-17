4c985eeb52d6b86b03a28ef4c920ffd56d4191ea
Sandeep Vadla <sandeep_vadla@merilytics.com>
2023-12-17 16:19:11 +0530

    Install packages

---
+++ $ 03-install packages.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+LOGFILE ="tmp/$0"
+VALIDATE() { 
+    if [$1 -ne 0]
+    then
+        echo -e "$2.. FAILED"
+    else
+        echo -e "$2... SUCCESS"
+    fi
+}
+
+if [$ID -ne 0 ]
+    then 
+       echo -e "please run the script with root acess"
+       exit 1
+    else
+       echo -e "you are root user"
+
+fi
+
+# git mysql
+for packages in $@
+do
+  yum list installed $packages &>> $LOGFILE
+
+  if [$? -ne 0]
+   then
+       yum install $packages -y $LOGFILE
+       VALIDATE $? "installing $package"
+    else
+      echo -e "$packages is alredy installed...
+done
+
