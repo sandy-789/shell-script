6a9c40ce6bf1ad1a8472aa375d4a041f9f5940a9
Sandeep Vadla <sandeep_vadla@merilytics.com>
2023-12-25 13:34:02 +0530

    roboshop-config

---
+++ delete-old-logs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SOUR_DIRE= /tmp/shell-scripts.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $SOUR_DIRE ]
+then
+    echo -e "$R Source directory: $SOUR_DIRE does not exits. "
+
+fi 
+
+FILES_TO_DELETE=$(find . -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+    echo "Deleting file: $line"
+    rm -rf $line
+done <<< $FILES_TO_DELETE
