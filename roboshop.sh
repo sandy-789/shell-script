d54961339c22f3a6acbce0b6585db430e17d61fc
Sandeep Vadla <sandeep_vadla@merilytics.com>
2023-12-25 18:18:59 +0530

    roboshop-config

--- roboshop.sh
+++ roboshop.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-0d797842c85825e84
+SG_ID=sg-0d797842c85825e84 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z029493526PC64DCTILZ3
-DOMIAN_NAME="prorb.online.com"
+ZONE_ID=Z029493526PC64DCTILZ3 # replace your zone ID
+DOMAIN_NAME="prorb.online"
 
 for i in "${INSTANCES[@]}"
 do
-   
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
@@ -19,6 +18,7 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0d797842c85825e84 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
+    #create R53 record, make sure you delete existing record
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
@@ -37,5 +37,4 @@ do
         }]
     }
         '
-
 done
