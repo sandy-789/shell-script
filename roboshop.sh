d1ab0770304cbf776da251f8bb0494925d1b9147
Sandeep Vadla <sandeep_vadla@merilytics.com>
2023-12-25 17:18:29 +0530

    roboshop-config

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,6 @@
 AMI=ami-03265a0778a880afb
 SG=sg-0d797842c85825e84
 INSATNCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=
 
 for i in "${INSTANCES[@]}"
 do
@@ -14,6 +13,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0d797842c85825e84 --
     echo "$i: $IP_ADDRESS"
 done
