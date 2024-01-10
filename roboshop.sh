f70b5935678fac835a2776a81dca4dfc5301e81e
Sandeep Vadla <sandeep_vadla@merilytics.com>
2024-01-10 22:01:33 +0530

    ansible

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-0d797842c85825e84 #replace with your SG ID
+SG_ID=sg-046a659bb87c329c9 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 ZONE_ID=Z029493526PC64DCTILZ3 # replace your zone ID
 DOMAIN_NAME="prorb.online"
@@ -15,7 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0d797842c85825e84 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-046a659bb87c329c9 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
     #create R53 record, make sure you delete existing record
